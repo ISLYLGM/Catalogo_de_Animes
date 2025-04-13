1c5dc239380e9b9e97da81d61e05a68e32baeee8
ISLYLGM <isabely.mts2000@gmail.com>
2025-04-12 22:13:01 -0300

    footer.php

--- anime-catalogo/footer.php
+++ anime-catalogo/footer.php
@@ -1,17 +1,15 @@
-
-<footer style="background-color: rgba(5, 9, 54, 0.84); color: white; padding: 40px 0 20px; text-align: center; width: 100%; position: relative;">
+<footer style="background-color: rgba(5, 9, 54, 0.84); color: white; padding: 40px 0 20px; text-align: center; position: fixed; bottom: 0; width: 100%;">
 
     <!-- Imagem decorativa sobre o footer -->
     <img src="img/imagem_2025-04-10_233638129-removebg-preview.png" alt="Decoração" style="
         position: absolute;
-        top: -650px;
+        top: -60px;
         left: 50%;
         transform: translateX(-50%);
         width: 100px;
         z-index: 10;
     ">
-
-    <p>&copy; 2025 Catálogo de Animes. Todos os direitos reservados.</p>
+<p>&copy; 2025 Catálogo de Animes. Todos os direitos reservados.</p>
     <p>
         <a href="sobre.php" style="color: white; text-decoration: none; margin-right: 20px;">Sobre</a>
         <a href="contato.php" style="color: white; text-decoration: none;">Contato</a>
